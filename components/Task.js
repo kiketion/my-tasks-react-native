95e324c627a4ffce362aadfdfb03c13efc31e646
kiketion <kiketionje@gmail.com>
2022-04-19 10:41:29 +0300

    task comp styled

--- components/Task.js
+++ components/Task.js
@@ -3,12 +3,49 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const Task = (props) => {
   return (
-    <View>
-      <Text>{props.text}</Text>
+    <View style={Styles.item}>
+      <View style={Styles.itemLeft}>
+        <View style={Styles.square}></View>
+        <Text style={Styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={Styles.circular}></View>
     </View>
   );
 };
 
-const Styles = StyleSheet.create({});
+const Styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#545B62',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: '80%',
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#545B62',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
 
 export default Task;
